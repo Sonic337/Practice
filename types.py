6376c7c356b662787cf1a0acd2a1fabe8125bc7d
Sonic <sonicthesaga1@gmail.com>
2026-05-27 23:29:00 +0530

    python data types

---
+++ types.py
@@ -0,0 +1,22 @@
+# strings
+name = "Sonic"
+
+# integers
+age = 20
+
+# floats
+height = 5.9
+
+# booleans
+is_employed = True
+
+# lists
+skills = ["git", "terminal", "python"]
+
+# dictionaries
+person = {"name": "Sonic", "age": 20}
+
+print(name, age, height, is_employed)
+print(skills[0])
+print(person["name"])
+
