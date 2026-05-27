6f50a359c1863873dadad768b2ccb24dcbfd5fb0
Sonic <sonicthesaga1@gmail.com>
2026-05-27 23:32:10 +0530

    conditional and loops

---
+++ logic.py
@@ -0,0 +1,16 @@
+age = 20
+
+if age >= 18:
+    print("adult")
+else:
+    print("minor")
+
+skills = ["git", "terminal", "python"]
+
+for skill in skills:
+    print("I know " + skill)
+
+count = 0
+while count < 3:
+    print("count is " + str(count))
+    count += 1
